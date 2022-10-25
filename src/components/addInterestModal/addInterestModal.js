6a8e4877302719a4193b20e1d35f1e14bd8aac4f
Camilo <camilochafloque@gmail.com>
2022-10-25 18:26:03 -0500

    Added interests to users in Firebase

--- src/components/addInterestModal/addInterestModal.js
+++ src/components/addInterestModal/addInterestModal.js
@@ -1,30 +1,49 @@
+import React, { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 import Form from "react-bootstrap/Form";
+import { useProfile } from "../../utilities/userProfile";
+import { useDbUpdate } from "../../utilities/firebase";
 
 const AddInterestModal = ({ show, handleClose }) => {
+  const [user, error, isLoading] = useProfile();
+  const [updateData, result] = useDbUpdate("/");
+  const [interest, setInterest] = useState();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    //TODO: CHECK VALIDATION
+    if (interest.length > 0) {
+      const data = {
+        name: interest,
+      };
+      updateData({ ["/users/" + user.uid + "/interests/" + uuidv4()]: data });
+    }
+    handleClose();
+  };
+
   return (
     <Modal show={show} onHide={handleClose}>
       <Modal.Header closeButton>
         <Modal.Title>Add an interest</Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <div>
-            <Form.Label htmlFor="interestName">Interest name</Form.Label>
-            <Form.Control
-            type=""
+        <Form>
+          <Form.Label htmlFor="interestName">Interest name</Form.Label>
+          <Form.Control
+            type="text"
             id="interestName"
-            />
-            <Form.Text id="" muted>
-            Enter the name of your interest
-            </Form.Text>
-        </div>        
+            onChange={(e) => setInterest(e.target.value)}
+          />
+          <Form.Text muted>Enter the name of your interest</Form.Text>
+        </Form>
       </Modal.Body>
       <Modal.Footer>
         <Button variant="secondary" onClick={handleClose}>
           Close
         </Button>
-        <Button variant="primary" onClick={handleClose}>
+        <Button variant="primary" onClick={(e) => handleSubmit(e)}>
           Save Changes
         </Button>
       </Modal.Footer>
