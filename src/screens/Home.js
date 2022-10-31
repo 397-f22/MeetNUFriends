38188b31db1f6b498fa7abf69b584139f55a61db
derrickrosefan <114096803+derrickrosefan@users.noreply.github.com>
2022-10-31 00:51:25 -0500

    fix add interest workflow

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,14 +1,11 @@
 import React, { useState } from "react";
 import { Button, ListGroup, Badge } from "react-bootstrap";
-import { useNavigate } from "react-router-dom";
 import { useProfile } from "../utilities/userProfile";
 import { signOut, useDbData } from "../utilities/firebase";
 import AddInterestModal from "../components/addInterestModal/addInterestModal";
 import UserCard from "../components/userCard/userCard";
 
 const Home = () => {
-  const navigate = useNavigate();
-
   const [currentUser, error, isLoading] = useProfile();
   const [show, setShow] = useState(false);
   const [users, errorUsers, isLoadingUsers] = useDbData("/users");
@@ -20,7 +17,7 @@ const Home = () => {
     return <h1>Error loading users data: {`${error}`}</h1>;
   if (isLoading || isLoadingUsers) return <h1>Loading users data...</h1>;
 
-  if (!currentUser) navigate("/login");
+  if (!currentUser) return;
   if (!users) return <div> No Users </div>;
 
   const currentUserInformation = Object.entries(users).filter(
