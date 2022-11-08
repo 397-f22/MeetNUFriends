43960975c1fea2638295205e9762f9881d24e221
ShaliniR8 <61011528+ShaliniR8@users.noreply.github.com>
2022-11-07 20:48:55 -0600

    Update calculate.js

--- src/utilities/calculate.js
+++ src/utilities/calculate.js
@@ -9,25 +9,12 @@ const stem = (string) => {
     return stemmer(string)
 }
 
-export const getBigrams = (string) =>{
-    var s = string.toLowerCase()
-    var v = s.split(''); 
-    for(var i=0; i<v.length; i++){
-        v[i] = s.slice(i, i + 2); 
-    } 
-    return v; 
-} 
-
 export const stringSimilarity = (str1, str2) =>{ 
+    // Lowercase -> tokenize -> stem -> ratio of number of matching stems b/w two strings
     if(str1.length>0 && str2.length>0){ 
         const stems1 = tokenize(str1.toLocaleLowerCase()).map(word => stem(word))
         const stems2 = tokenize(str2.toLocaleLowerCase()).map(word => stem(word))
-        console.log('-------')
-        console.log(stems1)
-        console.log(stems2)
-        console.log('-------')
         const matches = stems1.filter(stem => stems2.includes(stem))
-        console.log(matches.length/(stems1.length > stems2.length? stems1.length: stems2.length))
         return matches.length/(stems1.length > stems2.length? stems1.length: stems2.length)
 
         
